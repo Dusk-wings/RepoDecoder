d9ced160e139487ad0b1f6bd97a7864442d58776
Dusk-wings <pantkaran05@gmail.com>
2026-09-20 08:47:36 +0530

    feat: Implement FastAPI server with CORS middleware and server status endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from app.core.config import env_config
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from datetime import datetime
+
+app = FastAPI()
+
+allowed_origins = []
+if env_config.ALLOWED_ORIGIN:
+    allowed_origins = env_config.ALLOWED_ORIGIN.split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/server-status", tags=["Server Status"])
+async def validate_user():
+    return JSONResponse(
+        status_code=200,
+        content={"message": "Hello, From the server", "time": f"{datetime.now()}"},
+    )
